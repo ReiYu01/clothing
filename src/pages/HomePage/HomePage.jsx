2bf560a3e0c5ae0c927755a40013e58604a971bd
瑞瑜 <ruiyu@ruiyude-MacBook-Pro.local>
2020-09-07 12:13:53 +0800

    add

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//scss
+import "./HomePage.scss";
+
+//components
+import Directory from "../../components/directory/directory";
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+);
+
+export default HomePage;
